c1baef0ee6a1d72e813ead7260713d3bab5aeb1c
phund <itghost90@gmail.com>
2015-10-28 16:40:14 +0700

    Step 4 completed

--- app.ng.js
+++ app.ng.js
@@ -5,5 +5,14 @@ if (Meteor.isClient) {
 
   angular.module('socially').controller('PartiesListCtrl', ['$scope', '$meteor', function($scope, $meteor){
     $scope.parties = $meteor.collection(Parties);
+
+    $scope.remove = function(party){
+      $scope.parties.remove(party);
+    };
+
+    $scope.removeAll = function(){
+      $scope.parties.remove();
+    };
+
   }])
 }
