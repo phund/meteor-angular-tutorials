a64e5fb9104f2e34ebdab68e52cdfa043beac197
phund <phund@elarion.com>
2015-10-28 21:07:06 +0700

    Step 8 completed

--- client/routes.ng.js
+++ client/routes.ng.js
@@ -1,3 +1,13 @@
+angular.module("socially").run(function ($rootScope, $state) {
+  $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+    // We can catch the error thrown when the $requireUser promise is rejected
+    // and redirect the user back to the main page
+    if (error === 'AUTH_REQUIRED') {
+      $state.go('parties');
+    }
+  });
+});
+
 angular.module('socially').config(function($urlRouterProvider, $stateProvider, $locationProvider){
  
   $locationProvider.html5Mode(true);
@@ -11,7 +21,12 @@ angular.module('socially').config(function($urlRouterProvider, $stateProvider, $
     .state('partyDetails', {
       url: '/parties/:partyId',
       templateUrl: 'client/parties/views/party-details.ng.html',
-      controller: 'PartyDetailsCtrl'
+      controller: 'PartyDetailsCtrl',
+      resolve: {
+        "currentUser": function ($meteor) {
+          return $meteor.requireUser();
+        }
+      }
     });
 
   $urlRouterProvider.otherwise("/parties");
