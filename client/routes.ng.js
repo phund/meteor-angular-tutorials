bb623e7e2c116428dfe2c9920691bbc6363ebbe1
phund <itghost90@gmail.com>
2015-10-30 14:03:46 +0700

    Step 18

--- client/routes.ng.js
+++ client/routes.ng.js
@@ -17,13 +17,33 @@ angular.module("socially").config(function ($urlRouterProvider, $stateProvider, 
       templateUrl: 'client/parties/views/parties-list.ng.html',
       controller: 'PartiesListCtrl'
     })
-    .state('partyDetails', {
-      url: '/parties/:partyId',
-      templateUrl: 'client/parties/views/party-details.ng.html',
-      controller: 'PartyDetailsCtrl',
+    .state('login', {
+      url: '/login',
+      templateUrl: 'client/users/views/login.ng.html',
+      controller: 'LoginCtrl',
+      controllerAs: 'lc'
+    })
+    .state('register',{
+      url: '/register',
+      templateUrl: 'client/users/views/register.ng.html',
+      controller: 'RegisterCtrl',
+      controllerAs: 'rc'
+    })
+    .state('resetpw', {
+      url: '/resetpw',
+      templateUrl: 'client/users/views/reset-password.ng.html',
+      controller: 'ResetCtrl',
+      controllerAs: 'rpc'
+    })
+    .state('logout', {
+      url: '/logout',
       resolve: {
-        "currentUser": function ($meteor) {
-          return $meteor.requireUser();
+        "logout": function($meteor, $state) {
+          return $meteor.logout().then(function(){
+            $state.go('parties');
+          }, function(err){
+            console.log('logout error - ', err);
+          });
         }
       }
     });
