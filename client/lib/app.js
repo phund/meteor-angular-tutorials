4c41703d90e56d10024a27ad340d7f0a35643db4
phund <itghost90@gmail.com>
2016-01-04 14:13:19 +0700

    Step 11 - Running your app on Android or iOS with PhoneGap

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,7 +1,16 @@
-if (Meteor.isClient) {
-  angular.module('socially', [
-    'angular-meteor', 
-    'ui.router',
-    'accounts.ui'
-  ]);
+angular.module('socially', [
+  'angular-meteor', 
+  'ui.router',
+  'accounts.ui'
+]);
+ 
+function onReady() {
+  angular.bootstrap(document, ['socially'], {
+    strictDi: true
+  });
 }
+ 
+if (Meteor.isCordova)
+  angular.element(document).on("deviceready", onReady);
+else
+  angular.element(document).ready(onReady);
