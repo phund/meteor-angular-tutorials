7b50e6775ace1976fc502c6ea4b9ea892c74d0e1
phund <itghost90@gmail.com>
2016-01-07 10:20:05 +0700

    Step 12 - Search, sort, pagination and reactive vars

--- client/parties-list/parties-list.ts
+++ client/parties-list/parties-list.ts
@@ -1,5 +1,6 @@
 /// <reference path="../../typings/angular2-meteor.d.ts" />
 /// <reference path="../../typings/meteor-accounts-ui.d.ts" />
+/// <reference path="../../typings/ng2-pagination.d.ts" />
 
 import {Component, View} from 'angular2/core';
 
@@ -13,18 +14,24 @@ import {AccountsUI} from 'meteor-accounts-ui';
 
 import {MeteorComponent} from 'angular2-meteor';
 
+import {PaginationService, PaginatePipe, PaginationControlsCpm} from 'ng2-pagination';
+
 @Component({
-    selector: 'parties-list'
+    selector: 'parties-list',
+    viewProviders: [PaginationService]
 })
 @View({
     templateUrl: '/client/parties-list/parties-list.html',
-    directives: [PartiesForm, RouterLink, AccountsUI]
+    directives: [PartiesForm, RouterLink, AccountsUI, PaginationControlsCpm],
+    pipes: [PaginatePipe]
 })
 export class PartiesList extends MeteorComponent {
     parties: Mongo.Cursor<Party>;
-    pageSize: number = 10;
+    pageSize: number = 1;
     curPage: ReactiveVar<number> = new ReactiveVar<number>(1);
-    nameOrder: number = 1;
+    nameOrder: ReactiveVar<number> = new ReactiveVar<number>(1);
+    partiesSize: number = 0;
+    location: ReactiveVar<string> = new ReactiveVar<string>(null);
 
     constructor() {
         super();
@@ -32,12 +39,16 @@ export class PartiesList extends MeteorComponent {
             let options = {
                 limit: this.pageSize,
                 skip: (this.curPage.get() - 1) * this.pageSize,
-                sort: { name: this.nameOrder }
+                sort: { name: this.nameOrder.get() }
             };
-            this.subscribe('parties', options, () => {
-                this.parties = Parties.find({}, { sort: { name: this.nameOrder } });
+            this.subscribe('parties', options, this.location.get(), () => {
+                this.parties = Parties.find({}, { sort: { name: this.nameOrder.get() } });
             }, true);
         });
+
+        this.autorun(() => {
+            this.partiesSize = Counts.get('numberOfParties');
+        }, true);
     }
 
     removeParty(party) {
@@ -45,10 +56,16 @@ export class PartiesList extends MeteorComponent {
     }
 
     search(value) {
-        if (value) {
-            this.parties = Parties.find({ location: value });
-        } else {
-            this.parties = Parties.find();
-        }
+        this.curPage.set(1);
+        this.location.set(value);
     }
+
+    onPageChanged(page: number) {
+        this.curPage.set(page);
+    }
+
+    changeSortOrder(nameOrder) {
+        this.nameOrder.set(parseInt(nameOrder));
+    }
+
 }
