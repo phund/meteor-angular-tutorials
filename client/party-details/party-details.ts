f399827c9e98dc73b7dd7c3987418b305f4badf9
phund <itghost90@gmail.com>
2016-01-07 10:48:08 +0700

    Step 13 - Using and creating AngularJS filters

--- client/party-details/party-details.ts
+++ client/party-details/party-details.ts
@@ -13,16 +13,20 @@ import {RequireUser} from 'meteor-accounts';
 
 import {MeteorComponent} from 'angular2-meteor';
 
+import {DisplayName} from 'client/lib/pipes';
+
 @Component({
     selector: 'party-details'
 })
 @View({
+    pipes: [DisplayName],
     templateUrl: '/client/party-details/party-details.html',
     directives: [RouterLink]
 })
 @RequireUser()
 export class PartyDetails extends MeteorComponent {
     party: Party;
+    users: Mongo.Cursor<Object>;
 
     constructor(params: RouteParams) {
         super();
@@ -30,19 +34,23 @@ export class PartyDetails extends MeteorComponent {
         this.subscribe('party', partyId, () => {
             this.party = Parties.findOne(partyId);
         }, true);
+
+        this.subscribe('uninvited', partyId, () => {
+            this.users = Meteor.users.find({ _id: { $ne: Meteor.userId() } });
+        }, true);
     }
 
     saveParty(party) {
-      if (Meteor.userId()) {
-        Parties.update(party._id, {
-          $set: {
-            name: party.name,
-            description: party.description,
-            location: party.location
-          }
-        });
-      } else {
-        alert('Please log in to change this party');
-      }
+        if (Meteor.userId()) {
+            Parties.update(party._id, {
+              $set: {
+                name: party.name,
+                description: party.description,
+                location: party.location
+              }
+            });
+        } else {
+            alert('Please log in to change this party');
+        }
     }
 }
